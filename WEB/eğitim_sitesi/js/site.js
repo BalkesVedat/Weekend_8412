086d742f70dddf31a74c07d0d56f66404e22a5f2
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-08-15 19:32:18 +0300

    15082026_1932

---
+++ WEB/eğitim_sitesi/js/site.js
@@ -0,0 +1,13 @@
+
+var deger = 0;
+function Eksilt() {
+   deger = document.getElementById("deger").innerHTML = deger - 1;
+}
+
+function Arttir() {
+   deger = document.getElementById("deger").innerHTML = deger + 1;
+}
+
+function Sifirla() {
+   deger = document.getElementById("deger").innerHTML = 0;
+}
